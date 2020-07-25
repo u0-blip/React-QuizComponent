a042a9bcc9d47f6a588c7fbc9e4dec7cb171877d
u0-blip <chenduona@gmail.com>
2020-07-25 19:53:49 +1000

    task 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
-import Quiz from './Quiz.js'
+
+import Quiz from './Quiz'
+import QuizQuestion from './QuizQuestion'
 
 class App extends Component {
   render() {
--- src/Quiz.js
+++ src/Quiz.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
+import QuizQuestion from './QuizQuestion'
 
 let quizData  = require('./quiz_data.json')
 
 class Quiz extends Component{
     render(){
         return (
-        <div className='QuizQuestion'>
-        {quizData.quiz_questions[0].instruction_text}
-        </div>)
+        <QuizQuestion 
+        quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}
+        />
+        )
     }
     constructor(props){
         super(props)
