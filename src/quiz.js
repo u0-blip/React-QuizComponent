5f207e4c4ab2b00f6d7946842eb345f65ef7ad03
u0-blip <chenduona@gmail.com>
2020-07-25 18:59:39 +1000

    naming issues

--- src/quiz.js
+++ src/quiz.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
 
-let quiz_data = require('./quiz_data.json')
+let quizData  = require('./quiz_data.json')
 
 class Quiz extends Component{
     render(){
         return (
-        <div className='quizQuestion'>
-        {quiz_data.quiz_questions[0].instruction_text}
+        <div className='QuizQuestion'>
+        {quizData.quiz_questions[0].instruction_text}
         </div>)
     }
     constructor(props){
